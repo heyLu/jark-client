9272aaaeb93b25b42a1cf3f7e9add6a0d0515214
Martin DeMello <martindemello@gmail.com>
2012-01-17 20:18:39 -0800

    pluginify modules.self

--- src/modules/self.ml
+++ src/modules/self.ml
@@ -9,47 +9,67 @@ module Self =
     module C = Config
     open Stat
     open Gopt
+    open Plugin
+
+    let registry = Plugin.create ()
+    let register_fn = Plugin.register_fn registry
+    let alias_fn = Plugin.alias_fn registry
 
     let usage =
       Gstr.unlines ["usage: jark [options] self <command> <args>";
                      "Available commands for 'self' module:\n";
-                     "    install     [--no-standalone] [--force]" ; 
+                     "    install     [--no-standalone] [--force]" ;
                      "    uninstall ";
                      "    update ";
                      "    status      VM connection status"]
 
-    let show_usage () = Gstr.pe usage
+    let show_usage args = Gstr.pe usage
 
-    let install () =
-      (try Unix.mkdir Config.cljr 0o740 with Unix.Unix_error(Unix.EEXIST,_,_) -> ());
-      (try Unix.mkdir C.cljr_lib 0o740 with Unix.Unix_error(Unix.EEXIST,_,_) -> ());
+    let install_standalone_jar () =
+      let st_jar = (C.jar "standalone") in
+      if Gfile.exists st_jar then
+        Gstr.pe (st_jar ^ " already exists")
+        (* TODO: why do we care? *)
+      else
+        C.install_standalone ()
+
+    let install args =
+      let mkdir dir =
+        try Unix.mkdir dir 0o740 with Unix.Unix_error (Unix.EEXIST,_,_) -> ()
+      in
+      mkdir Config.cljr;
+      mkdir C.cljr_lib;
       C.setup_cljr ();
-      if C.standalone then begin
-        if (Gfile.exists (C.jar "standalone")) then
-          Gstr.pe ((C.jar "standalone") ^ " already exists")
-        else
-          C.install_standalone()
-      end
-      else 
-        C.install_components();
+      match C.standalone with
+        true -> install_standalone_jar ()
+      | false -> C.install_components ();
       Gstr.pe "Installed components successfully"
 
-    let status () =
+    let status args =
       let host = Gopt.getopt "--host" () in
       let port = Gopt.getopt "--port" () in
-      Gstr.pe (Gstr.unlines ["PID      " ^ Stat.get_pid();
+      Gstr.pe (Gstr.unlines ["PID      " ^ Stat.get_pid ();
                              "Host     " ^ host;
                              "Port     " ^ port])
 
-    let uninstall () =
+    let uninstall args =
       Gstr.pe "Removed jark configs successfully"
+
+    let _ =
+      register_fn "usage" show_usage [];
+
+      register_fn "install" install [
+        "[--no-standalone] [--force]";
+        "Install jark"];
+
+      register_fn "status" status ["VM connection status"];
+
+      register_fn "uninstall" uninstall ["Uninstall jark"];
+
+      alias_fn "usage" ["help"]
 
     let dispatch cmd arg =
       Gopt.opts := (Glist.list_to_hashtbl arg);
-      match cmd with
-      | "usage"   -> Gstr.pe usage
-      | "install" -> install ()
-      | "status"  -> status()
-      |  _        -> Gstr.pe usage
+      Plugin.dispatch registry cmd arg
 
 end
