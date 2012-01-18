5eda23aab49c34e33cc8bcb911babce67cd3a075
Martin DeMello <martindemello@gmail.com>
2012-01-17 20:51:36 -0800

    pluginify swank

--- src/modules/swank.ml
+++ src/modules/swank.ml
@@ -6,6 +6,11 @@ module Swank =
     open Jark
     open Config
     open Gopt
+    open Plugin
+
+    let registry = Plugin.create ()
+    let register_fn = Plugin.register_fn registry
+    let alias_fn = Plugin.alias_fn registry
 
     let usage =
       Gstr.unlines ["usage: jark [options] swank <command> <args>";
@@ -14,17 +19,23 @@ module Swank =
                      "              Start a swank server on given port\n" ;
                      "    stop      Stop an instance of the server"]
 
-    let show_usage () = Gstr.pe usage
+    let show_usage args = Gstr.pe usage
 
-    let start () =
-      let port = Gopt.getopt "--swank-port" () in 
+    let start args =
+      let port = Gopt.getopt "--swank-port" () in
       Jark.nfa "jark.swank" ~f:"start" ~a:["0.0.0.0"; port] ()
+
+    let _ =
+      register_fn "usage" show_usage [];
+
+      register_fn "start" start [
+        "[-s|--swank-port 4005]" ;
+        "Start a swank server on given port\n"];
+
+      alias_fn "usage" ["help"]
 
     let dispatch cmd arg =
       Gopt.opts := (Glist.list_to_hashtbl arg);
-      match cmd with
-      | "usage"   -> Gstr.pe usage
-      | "start"   -> start()
-      |  _        -> Gstr.pe usage
+      Plugin.dispatch registry cmd arg
 
 end
