fec32bb699d168849e3e27d441f149f86267df67
Martin DeMello <martindemello@gmail.com>
2012-01-24 01:30:04 -0800

    make the doc module a straight passthrough

--- src/modules/doc.ml
+++ src/modules/doc.ml
@@ -1,27 +1,9 @@
 module Doc =
   struct
-
-    open Glist
-    open Gstr
     open Jark
-    open Config
-    open Plugin
 
-    let registry = Plugin.create ()
-    let register_fn = Plugin.register_fn registry
-    let alias_fn = Plugin.alias_fn registry
+    let show_usage args = ()
 
-    let show_usage args = Plugin.show_usage registry "doc"
-
-    let search args = Jark.nfa "jark.doc" ~f:"search" ~a:args ()
-
-    let examples args = Jark.nfa "jark.doc" ~f:"examples" ~a:args ()
-
-    let _ =
-      register_fn "search" search ["<term>"];
-
-      register_fn "examples" examples ["[--show-browser]"]
-
-    let dispatch cmd arg =
-      Plugin.dispatch registry cmd arg
+    let dispatch cmd args =
+      Jark.nfa "jark.doc" ~f:cmd ~a:args ()
 end
